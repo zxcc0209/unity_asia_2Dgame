22e4c36a63a3263bab3bbe5daee249eb0d4f3431
zxcc0209 <71679895+zxcc0209@users.noreply.github.com>
2020-11-10 15:59:26 +0800

    攝影機跟隨

---
+++ unity_asia_2Dgame/Assets/SCRIPTS/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void NextLevel(string newLV)
+    {
+        
+        SceneManager.LoadScene(newLV);
+
+    }
+    public void BacktoMenu()
+    {
+
+        SceneManager.LoadScene("選單");
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- unity_asia_2Dgame/Assets/SCRIPTS/Player.cs
+++ unity_asia_2Dgame/Assets/SCRIPTS/Player.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using UnityEngine.UI;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -6,11 +6,24 @@ public class Player : MonoBehaviour
     //修飾詞 類別 名稱
     //None 空直-程式錯誤
     public GameObject Final;
+    public Text textcount;
+    public int count;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         print("碰到傳送們了");
-        Final.SetActive(true);
 
+        if (collision.name == "傳送們")
+        {
+            Final.SetActive(true);
+        }
+
+        if (collision.tag == "gem")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textcount.text = count + "/999";
+        }
 
     }
 
